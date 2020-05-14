feac73a8d7e63b7563b24f6a7deeaad533dcb515
KhajaMoizUddin <40728759+KhajaMoizUddin@users.noreply.github.com>
2020-05-15 03:12:07 +0530

    EntityFrameworkMVC

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplicationMSDN.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is default Action Method";
+        }
+
+        public string Welcome()
+        {
+            return "This is Welcome Method";
+        }
+    }
+}
